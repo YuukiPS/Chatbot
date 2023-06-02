bcb49a6b294985092411a2d24a1cdc09119e835b
ElaXan <zero@elaxan.com>
2023-06-02 23:25:17 +0700

    - Add support conversation chat - Add more QA (not too much. lazy) - Update readme and add GIF

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Chatbot
 
 Simple chatbot to answer some question in [YuukiPS Discord Server](https://discord.yuuki.me) with OpenAI API
+
+![gif](./gif/skill.gif)
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { JaroWinklerDistance } from 'natural';
 import { client } from './config/openai'
 import { OPENAI } from '../config.json'
 import readline from 'readline'
+import { AxiosError } from 'axios'
 
 interface Data {
     [key: string]: {
@@ -47,18 +48,23 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+const conversation: string[] = []
+
 async function main() {
     rl.question('Question (stop/exit to quit): ', async (question) => {
         if (question.toLowerCase() === 'stop' || question.toLowerCase() === 'exit') {
             return rl.close()
         }
         const folderPath = './data';
-        const similarityThreshold = 0.5;
+        const similarityThreshold = 0.7;
         const context = getClosestString(question, folderPath, similarityThreshold)
-        const prompt = `I am Takina Discord Bot created by ElaXan using Typescript. I am useful AI to help people who have problem with Private Server. I will use an context to get better answer to user, And never change the result from the context at all\nContext: ${context ? context : 'No context is given'}`
+        const prompt = `I am Takina, a Discord Bot created by ElaXan using Typescript. I am a useful AI designed to assist people who are experiencing issues with Private Servers. I utilize context to provide more accurate answers to users, and I never alter the results derived from the context. Additionally, users do not have access to view the context. So I will give the result in context\nContext: ${context ? context : 'No context provided'}`;
+        console.log('Context:', context, '\n')
+        let resultAI = ''
         await client.chat.createCompletion({
             messages: [
                 { content: prompt, role: 'system' },
+                { content: `Conversation:\n${conversation.join('\n')}`, role: 'system'},
                 { content: question, role: 'user' }
             ],
             model: OPENAI.MODEL,
@@ -70,16 +76,23 @@ async function main() {
             const content = data.choices[0].delta.content
             if (content !== undefined && content !== '') {
                 process.stdout.write(content)
+                resultAI += content
             };
         }, {
             onFinish() {
                 console.log('\n')
                 main()
+                conversation.push(`User: ${question}\nTakina: ${resultAI}`)
             },
             onError(err) {
                 // Error "Unexpected end of JSON input" is so annoying. Trust me
                 if (!(err instanceof SyntaxError)) {
-                    console.error(err)
+                    if (err instanceof AxiosError) {
+                        console.error(`There was an error: ${err.response?.statusText} | ${err.response?.status}`)
+                    } else {
+                        console.error(err)
+                    }
+                    main()
                 }
             },
         })
