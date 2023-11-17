83cf0b0bbcf3ff8d01b1dae513a57f474a5ad797
ElaXan <zero@elaxan.com>
2023-11-17 08:42:58 +0700

    feat(findDocument.ts): add utility class for finding documents using OpenAI embeddings
    The `FindDocument` utility class is added to provide functionality for finding documents using OpenAI embeddings. It includes methods for embedding queries and retrieving similar documents based on the cosine similarity of embeddings.
    
    The class includes interfaces `EmbeddingCommand` and `EmbeddingQA` to define the structure of the data used for embedding and storing embeddings. The `embedding` method is implemented to accept a query and type ('qa' or 'command') and return an array of objects containing the data and similarity score.
    
    The `cosineSimilarity` method is implemented as a private helper function to calculate the cosine similarity between two arrays of numbers.
    
    The class is exported as the default export of the module.

---
+++ src/Utils/findDocument.ts
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import { client } from '../config/openai';
+
+interface EmbeddingCommand {
+    command: string
+    description: string
+    usage: string
+    type: string
+    embedding: number[]
+}
+
+interface EmbeddingQA {
+    question: string
+    answer: string
+    embedding: number[]
+}
+
+/**
+ * Utility class for finding documents using OpenAI embeddings.
+ */
+class FindDocument {
+    public static async embedding(query: string | string[], type: 'qa'): Promise<{ data: EmbeddingQA, score: number }[]>;
+    public static async embedding(query: string | string[], type: 'command'): Promise<{ data: EmbeddingCommand, score: number }[]>;
+    public static async embedding(query: string | string[], type: 'qa' | 'command'): Promise<{ data: EmbeddingQA | EmbeddingCommand, score: number }[]> {
+        const embedding = await client.embeddings.create({
+            input: query,
+            model: 'text-embedding-ada-002'
+        }).then((response) => response.data[0].embedding);
+
+        const pathDataset = () => {
+            switch (type) {
+                case 'qa':
+                    return './src/data/embeddingQA.json'
+                case 'command':
+                    return './src/data/embeddingCommand.json'
+            }
+        }
+
+        const jsonParse: EmbeddingCommand[] | EmbeddingQA[] = JSON.parse(fs.readFileSync(pathDataset(), 'utf-8'))
+
+        const similarityScores = jsonParse.map((data) => ({
+            data,
+            score: this.cosineSimilarity(embedding, data.embedding)
+        }));
+
+        const sortedData = similarityScores.sort((a, b) => b.score - a.score).filter((data) => data.score > 0.7).slice(0, 5)
+
+        return sortedData;
+    }
+
+    private static cosineSimilarity = (a: number[], b: number[]): number => {
+        const dotProduct = a.reduce((acc, val, i) => acc + val * b[i], 0);
+        const magnitudeA = Math.sqrt(a.reduce((acc, val) => acc + val ** 2, 0));
+        const magnitudeB = Math.sqrt(b.reduce((acc, val) => acc + val ** 2, 0));
+        return dotProduct / (magnitudeA * magnitudeB);
+    };
+}
+
+export default FindDocument;
